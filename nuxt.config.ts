237417ce99b333d0721507c70f4cdb72ac0e5400
TomasiakM <marcintomasiak00@gmail.com>
2024-04-17 11:48:25 +0200

    Add tailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    cssPath: "@/assets/css/main.css",
+    configPath: "tailwind.config.ts",
+  },
 });
---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+export default {
+  content: [
+    "components/**/*.{vue,js,ts}",
+    "layouts/**/*.vue",
+    "pages/**/*.vue",
+    "composables/**/*.{js,ts}",
+    "plugins/**/*.{js,ts}",
+    "App.{js,ts,vue}",
+    "app.{js,ts,vue}",
+    "Error.{js,ts,vue}",
+    "error.{js,ts,vue}",
+    "content/**/*.md",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF7431",
+        secondary: "#31BCFF",
+        background: "#F2F0EB",
+      },
+    },
+  },
+  plugins: [],
+  autoprefixer: {},
+};
