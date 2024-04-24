28d73e947cbba97cf7f8263a1f38c8169aa644fd
TomasiakM <marcintomasiak00@gmail.com>
2024-04-24 23:02:53 +0200

    Add activities pagination

---
+++ components/Pagination.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="flex items-center gap-1" v-if="totalPages > 1">
+    <button @click="prevPage" :class="{ disabled: !hasPrevPage }">
+      <SvgChevron class="w-5 h-5 rotate-180" />
+    </button>
+
+    <div v-if="!pageButtons.includes(1)" class="flex items-center gap-1">
+      <button @click="setPage(1)">
+        {{ 1 }}
+      </button>
+
+      <span>...</span>
+    </div>
+
+    <button
+      v-for="p in pageButtons"
+      :key="p"
+      :class="{ active: currentPage === p }"
+      @click="setPage(p)"
+    >
+      {{ p }}
+    </button>
+
+    <div
+      v-if="!pageButtons.includes(totalPages)"
+      class="flex items-center gap-1"
+    >
+      <span>...</span>
+
+      <button @click="setPage(totalPages)">
+        {{ totalPages }}
+      </button>
+    </div>
+
+    <button @click="nextPage" :class="{ disabled: !hasNextPage }">
+      <SvgChevron class="w-5 h-5" />
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface IProps {
+  currentPage: number;
+  items: number;
+  pageSize: number;
+}
+
+interface IEmits {
+  (e: "changePage", page: number): void;
+}
+
+const props = defineProps<IProps>();
+const emits = defineEmits<IEmits>();
+
+const maxPagesToShow = ref(5);
+
+const totalPages = computed(() => Math.ceil(props.items / props.pageSize));
+
+const hasNextPage = computed(() => {
+  return props.currentPage < totalPages.value;
+});
+
+const hasPrevPage = computed(() => {
+  return props.currentPage > 1;
+});
+
+const nextPage = () => {
+  if (props.currentPage + 1 > totalPages.value) {
+    return;
+  }
+
+  emits("changePage", props.currentPage + 1);
+};
+
+const prevPage = () => {
+  if (props.currentPage - 1 < 1) {
+    return;
+  }
+
+  emits("changePage", props.currentPage - 1);
+};
+
+const setPage = (page: number) => {
+  if (page < 1 || page > totalPages.value) {
+    return;
+  }
+
+  emits("changePage", page);
+};
+
+const pageButtons = computed(() => {
+  const pages = Math.min(maxPagesToShow.value, totalPages.value);
+  let firstPage = props.currentPage - Math.floor(pages / 2);
+  firstPage = Math.max(firstPage, 1);
+  firstPage = Math.min(firstPage, totalPages.value - pages + 1);
+
+  return [...Array(pages)].map((_, i) => firstPage + i);
+});
+</script>
+
+<style lang="postcss">
+button {
+  @apply w-8 h-8 flex items-center justify-center text-secondary bg-white rounded-full;
+}
+
+.active {
+  @apply bg-primary text-white pointer-events-none;
+}
+
+.disabled {
+  @apply text-gray-700 bg-gray-200 pointer-events-none;
+}
+</style>
--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -5,11 +5,20 @@
       :is-error="isError"
       @refresh="fetchAllActivities()"
     >
-      <div class="grid grid-cols-2 xl:grid-cols-3 gap-x-2 gap-y-4">
+      <div class="grid grid-cols-2 xl:grid-cols-3 gap-x-2 gap-y-4 mb-4">
         <ActivityItem
-          v-for="activity in activities"
+          v-for="activity in pagedActivities"
           :key="activity.stravaId"
           :activity="activity"
+        />
+      </div>
+
+      <div class="flex justify-center">
+        <Pagination
+          :current-page="currentPage"
+          :page-size="pageSize"
+          :items="activities.length"
+          @change-page="onPageChange"
         />
       </div>
     </DataContainer>
@@ -17,7 +26,49 @@
 </template>
 
 <script lang="ts" setup>
+const route = useRoute();
+const router = useRouter();
+
+const page = Number(route.query.page);
+const currentPage = ref(page < 1 ? 1 : page || 1);
+const pageSize = ref(12);
+
 const { isLoading, isError, activities } = storeToRefs(useActivitiesStore());
 
 const { fetchAllActivities } = useActivitiesStore();
+watch(
+  () => activities.value,
+  () => {
+    if (isLoading.value || isError.value) {
+      return;
+    }
+
+    const pages = Math.ceil(activities.value.length / pageSize.value);
+    if (page > pages) {
+      currentPage.value = pages;
+      return router.replace({ query: { page: pages } });
+    }
+
+    if (page <= 1) {
+      return router.replace({ query: { page: undefined } });
+    }
+  }
+);
+
+const pagedActivities = computed(() =>
+  activities.value.slice(
+    (currentPage.value - 1) * pageSize.value,
+    currentPage.value * pageSize.value
+  )
+);
+
+const onPageChange = (page: number) => {
+  currentPage.value = page;
+
+  if (page !== 1) {
+    router.push({ query: { page } });
+  } else {
+    router.push({ query: { page: undefined } });
+  }
+};
 </script>
